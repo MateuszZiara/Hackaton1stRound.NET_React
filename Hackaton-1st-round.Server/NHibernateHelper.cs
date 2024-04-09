f80b67beaaa1bae1ccc502c045f919bd331c6483
Mateusz Ziara <ziaramateusz@gmail.com>
2024-04-09 02:32:10 +0200

    NHibernateHelper Configuration with Program.cs

---
+++ Hackaton-1st-round.Server/NHibernateHelper.cs
@@ -0,0 +1,40 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Hackaton_1st_round.Server;
+
+public class NHibernateHelper
+{
+    private static ISessionFactory _sessionFactory;
+
+    public static NHibernate.ISession OpenSession()
+    {
+        return SessionFactory.OpenSession();
+    }
+
+    private static ISessionFactory SessionFactory
+    {
+        get
+        {
+            if (_sessionFactory == null)
+            {
+                _sessionFactory = Fluently.Configure()
+                    .Database(
+                        MsSqlConfiguration.MsSql2012.ConnectionString(
+                            "Server=localhost\\SQLEXPRESS;Database=Kwiaciarnia;Integrated Security=SSPI;Application Name=Kwiaciarnia;TrustServerCertificate=true;")
+                    ) //TODO NAZWA DLA BAZY DANYCH NA TEN MOMENT TO Kwiaciarnia 
+                    /* .Mappings(m =>
+                         m.FluentMappings.AddFromAssemblyOf<KlientEntity>()
+                     ) Przykład mapowania TODO NIE ZAPOMINAĆ O MAPOWANIACH KOLEDZY*/
+                    .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                    .BuildSessionFactory();
+
+
+            }
+
+            return _sessionFactory;
+        }
+    }
+}
--- Hackaton-1st-round.Server/Program.cs
+++ Hackaton-1st-round.Server/Program.cs
@@ -1,14 +1,72 @@
+using System;
+using System.Reflection;
+using System.Security.Claims;
+using FluentAssertions.Common;
+using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Adding CORS headers
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:5173");
+            builder.AllowCredentials();
+            builder.AllowAnyHeader();
+            builder.AllowAnyMethod();
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+/*
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(
+        "Server=localhost\\SQLEXPRESS;Database=Kwiaciarnia;Integrated Security=SSPI;Application Name=Kwiaciarnia; TrustServerCertificate=true;"));
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AspNetUsers>()
+    .AddEntityFrameworkStores<DataContext>();
+*/
+builder.Services.AddFluentMigratorCore() // Move FluentMigrator registration here
+    .ConfigureRunner(c =>
+    {
+        c.AddSqlServer2016()
+            .WithGlobalConnectionString("Server=localhost\\SQLEXPRESS;Database=Hackaton;Integrated Security=SSPI;Application Name=Hackaton; TrustServerCertificate=true;")
+            .ScanIn(Assembly.GetExecutingAssembly()).For.All();
+    })
+    .AddLogging(config => config.AddFluentMigratorConsole());
 
 var app = builder.Build();
+//app.MapIdentityApi<AspNetUsers>();
+using var scope = app.Services.CreateScope();
+//var migrator = scope.ServiceProvider.GetService<IMigrationRunner>();
 
+//if (migrator != null)
+//{
+   // migrator.ListMigrations();
+   // migrator.MigrateUp();
+//}
+//else
+//{
+  //  throw new Exception("Migration fault");
+//}
+app.UseCors("AllowAllOrigins");
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
@@ -28,3 +86,8 @@ app.MapControllers();
 app.MapFallbackToFile("/index.html");
 
 app.Run();
+
+/*class DataContext : IdentityDbContext<AspNetUsers>
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options){}
+}*/
