8ef2d2fcee96ce20e6b43c1d295fa48f54ea1f30
Mateusz Ziara <ziaramateusz@gmail.com>
2024-04-09 17:08:54 +0200

    AspNetUsersContoller added to Api

---
+++ Hackaton-1st-round.Server/Controllers/AspNetUsers/AspNetUsersController.cs
@@ -0,0 +1,98 @@
+﻿namespace Hackaton_1st_round.Server.Controllers.AspNetUsers;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+
+    [EnableCors("AllowAllOrigins")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AspNetUsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Models.AspNetUsers.AspNetUsers>> GetAll()
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                var aspNetUsers = session.Query<Models.AspNetUsers.AspNetUsers>().ToList();
+                return Ok(aspNetUsers);
+            }
+        }
+
+        [HttpGet("id/{id}")]
+        public ActionResult<Models.AspNetUsers.AspNetUsers> GetById(Guid id)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                var aspNetUsers = session.Get<Models.AspNetUsers.AspNetUsers>(id);
+                if (aspNetUsers == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(aspNetUsers);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Models.AspNetUsers.AspNetUsers> CreateAddressEntity([FromBody] Models.AspNetUsers.AspNetUsers aspNetUsers)
+        {
+            if (aspNetUsers == null)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Save(aspNetUsers);
+                        transaction.Commit();
+                        return CreatedAtAction(nameof(GetById), new { id = aspNetUsers.Id }, aspNetUsers);
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+                    }
+                }
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteAddressEntity(Guid id)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        var aspNetUsers = session.Get<Models.AspNetUsers.AspNetUsers>(id);
+
+                        if (aspNetUsers == null)
+                        {
+                            return NotFound();
+                        }
+
+
+                        session.Delete(aspNetUsers);
+
+
+                        transaction.Commit();
+
+                        return NoContent();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        transaction.Rollback();
+                        return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+    }
